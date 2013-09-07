778ac1326e227754edca5da0d268824c7013ee68
Nao Minami <gurahu2htsubasa@gmail.com>
2013-09-07 15:37:45 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nawabari::Application.config.secret_token = '03b7898504ac63b708900a9fafdf74fe8d93b982fe3e3f7e105e7863467159c031efca7124020013451f69e826d762cd578e2e446b728e30512aa9a911aa042a'
