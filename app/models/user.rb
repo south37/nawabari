ee5d8d14f72da72d0e014d21880e061a113ea033
Nao Minami <gurahu2htsubasa@gmail.com>
2013-09-09 00:54:42 +0900

    usersのfoursq_idに1以上とuniquenessのvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :foursq_id, :territory
 
   validates :foursq_id, :territory, presence: true
-  validates :foursq_id, numericality: { only_integer: true }
+  validates :foursq_id, numericality: { only_integer: true, greater_than: 0 }
+  validates :foursq_id, uniqueness: true
   validates :territory, numericality: { greater_than_or_equal_to: 0 }
 end
