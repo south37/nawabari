91a615b572faaa601c46f8e5289279494f363582
Nao Minami <gurahu2htsubasa@gmail.com>
2013-10-05 00:55:25 +0900

    user modelのメソッド名を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # GET /users/rank/:id
   def rank
-    user = User.find_user_by_foursq_id params[:id]
+    user = User.find_by_foursq_id params[:id]
     if user
       area = user.area
       superior_users = User.find(:all, :conditions => ["area > :area", { area: area }])
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   # GET /user/update/:id/:name/:area
   def update
-    user = User.find_user_by_foursq_id params[:id]
+    user = User.find_by_foursq_id params[:id]
     user_params = { foursq_id: params[:id], name: params[:name], area: params[:area] }
     
     if ( (user && user.update_attributes(user_params)) || User.new(user_params).save )
