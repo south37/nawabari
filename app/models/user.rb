9c9f8ad741a33cab1b985cb34f06adc0b75690fd
Nao Minami <gurahu2htsubasa@gmail.com>
2013-10-06 04:07:13 +0900

    不要なメソッドを消去

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,4 @@ class User < ActiveRecord::Base
   validates :foursq_id, numericality: { only_integer: true, greater_than: 0 }
   validates :foursq_id, uniqueness: true
   validates :area, numericality: { greater_than_or_equal_to: 0 }
-
-  def self.find_by_foursq_id(id)
-    self.find(:first, :conditions => ["foursq_id = :foursq_id", { foursq_id: id }])
-  end
 end
