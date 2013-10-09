4633591c2c4db8e7f858dc0953d227075237a6bb
Nao Minami <gurahu2htsubasa@gmail.com>
2013-10-10 01:20:21 +0900

    routingにおける正規表現を改善

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Nawabari::Application.routes.draw do
   post   'territories',     to: 'territories#create'
 
   get 'users/rank/:id',      to: 'users#rank'
-  get 'users/update/:id/:name/:area' => 'users#update', area: /.*/
+  get 'users/update/:id/:name/:area' => 'users#update', area: /\d+(\.\d+)?/
   get 'users',               to: 'users#index'
   get 'users/ranking/:num/', to: 'users#ranking'
   delete 'users/:id', to: 'users#destroy', as: 'user'
